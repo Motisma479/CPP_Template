199ebece7f603de7ab4a176b4d1b7a2922a4dcc3
Mathieu Robion <robion.mathieu.16@gmail.com>
2026-02-20 12:09:01 +0100

    [Fix] readd the _DEBUG check on the leak detector

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,12 +1,12 @@
 #include "pch.hpp"
 
-#if defined(_MSC_VER) //&& defined(_DEBUG)
+#if defined(_MSC_VER) && defined(_DEBUG)
 #include <crtdbg.h>
 #endif
 
 int main(int argc, char** argv)
 {
-#if defined(_MSC_VER)// && defined(_DEBUG)
+#if defined(_MSC_VER) && defined(_DEBUG)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	// this row is used to stop at new/malloc N 284
 	//_CrtSetBreakAlloc(284);
